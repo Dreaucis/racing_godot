0149b095efb68a9ba4facf669ac9998a7f884d75
axel sarlin <ax.flumm@gmail.com>
2021-08-28 20:10:29 +0200

    Main menu picture

--- main_menu.gd
+++ main_menu.gd
@@ -1,9 +1,5 @@
 extends Control
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
     pass
@@ -11,3 +7,6 @@ func _ready():
 func start_button_pressed():
     print("start button pressed")
     get_tree().change_scene("World.tscn")
+
+func quit_button_pressed():
+    get_tree().quit()
